522cecf09979c4d33d32484d557e40dce8a8fd85
rconner46 <rconner@applause.com>
2024-10-15 10:43:23 -0500

    Update node and dependencies

--- src/hooks.ts
+++ src/hooks.ts
@@ -2,7 +2,7 @@ import { IWorld } from '@cucumber/cucumber';
 
 export const APPLAUSE_SESSION_ID_ATTACHMENT = 'applause-session-id';
 
-export function linkSessionId(this: IWorld<any>, sessionId: string) {
+export function linkSessionId(this: IWorld<unknown>, sessionId: string) {
   this.attach(sessionId, {
     fileName: APPLAUSE_SESSION_ID_ATTACHMENT,
     mediaType: 'text/plain',
